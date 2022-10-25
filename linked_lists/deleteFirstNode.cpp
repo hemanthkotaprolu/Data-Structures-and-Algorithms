e5908a9640cd7f5b255f48a0e1737619ce0a396f
hemanthkotaprolu <kotaproluhemanth@gmail.com>
2022-10-25 20:42:29 +0530

    add delete first node

---
+++ linked_lists/deleteFirstNode.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file deleteFirstNode.cpp
+ * @author Kotaprolu Hemanth (kotaproluhemanth@gmail.com)
+ * @brief Given head pointer to the linked list delete the first node of the linked list.
+ * @version 0.1
+ * @date 2022-10-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void printList(Node *head) {
+    
+    if(head == NULL) {
+        cout << "Nothing to print" << endl;
+        return;
+    }
+    
+    Node *temp = head;
+    
+    while(temp != NULL) {
+        
+        cout << temp->data << " ";
+        
+        temp = temp->next;
+    }
+    
+    cout << endl;
+}
+
+Node *insertAtBegin(Node *head, int x) {
+    Node *temp = new Node(x);
+    
+    temp->next = head;
+    
+    head = temp;
+    
+    return temp;
+}
+
+Node *deleteFirstNode(Node *head) {
+    if(head == NULL) {
+        return head;
+    }
+    
+    Node *temp = head->next;
+    
+    head->next = NULL;
+    head = temp;
+    
+    return temp;
+}
+
+int main() {
+    
+    Node *head = new Node(10);
+    
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    
+    head = insertAtBegin(head, 5);
+    
+    cout << "----------------Before Deleting----------------" << endl;
+    printList(head);
+    
+    cout << "----------------After Deleting----------------" << endl;
+    head = deleteFirstNode(NULL);
+    printList(head);
+    
+    return 0;
+}
